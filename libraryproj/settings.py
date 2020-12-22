399b940acd5ada5b8e95699ac16a3fb26377ac72
pkowalczyk1 <piotrek12kowalczyk@gmail.com>
2020-12-22 18:31:06 +0100

    Django heroku

--- libraryproj/settings.py
+++ libraryproj/settings.py
@@ -117,5 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+import django_heroku
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
